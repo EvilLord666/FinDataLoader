a05089af9b2262f4d2d7a5575524ac1b7e4800ca
Ushakov Michael <um.nix.user@gmail.com>
2021-08-17 14:35:32 +0500

    test was added

--- FinDataLoader/FinApiIntegrations.Tests/Yahoo/TestYahooFinanceLoaderService.cs
+++ FinDataLoader/FinApiIntegrations.Tests/Yahoo/TestYahooFinanceLoaderService.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
+using Microsoft.Extensions.Logging;
+using FinApiIntegrations;
+using FinApiIntegrations.Data;
+using FinApiIntegrations.Yahoo;
 
 namespace FinApiIntegrations.Tests.Yahoo
 {
     public class TestYahooFinanceLoaderService
     {
-        [Fact]
-        public void TestLoadAsync()
-        { 
-        
+        [Theory]
+        [InlineData("2y", "1d", "quotes")]
+        public void TestLoadAsync(string range, string interval, string indicators)
+        {
+            Task<MarketSelection> loadTask = _loader.LoadAsync(range, interval, indicators);
+            loadTask.Wait();
+            MarketSelection selection = loadTask.Result;
+
+            Assert.NotNull(selection);
         }
+
+        private IFinDataLoaderService _loader = new YahooFinanceLoaderService(new LoggerFactory());
     }
 }
