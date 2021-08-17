85bcc966544454106f7956856bbc624aaae36d4e
Ushakov Michael <um.nix.user@gmail.com>
2021-08-17 13:47:45 +0500

    test project created

---
+++ FinDataLoader/FinApiIntegrations.Tests/Yahoo/TestYahooFinanceLoaderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FinApiIntegrations.Tests.Yahoo
+{
+    public class TestYahooFinanceLoaderService
+    {
+        [Fact]
+        public void TestLoadAsync()
+        { 
+        
+        }
+    }
+}
