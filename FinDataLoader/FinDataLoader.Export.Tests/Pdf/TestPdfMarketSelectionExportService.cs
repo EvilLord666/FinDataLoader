7c94cb8c7b55a0795ed0cb3752b92cd76a166609
Ushakov Michael <um.nix.user@gmail.com>
2021-08-19 23:39:06 +0500

    test shows that solution is not working

--- FinDataLoader/FinDataLoader.Export.Tests/Pdf/TestPdfMarketSelectionExportService.cs
+++ FinDataLoader/FinDataLoader.Export.Tests/Pdf/TestPdfMarketSelectionExportService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using FinDataLoader.Common.Data;
 using FinDataLoader.Export;
 using FinDataLoader.Export.Pdf;
 using Microsoft.Extensions.Logging;
@@ -12,8 +14,18 @@ namespace FinDataLoader.Export.Tests.Pdf
     {
         [Fact]
         public void TestExportAsyncOnePageData()
-        { 
-            
+        {
+            MarketSelection data = new MarketSelection();
+            data.Timestamps.Add(new DateTime());
+            data.Open.Add(100);
+            data.Close.Add(90);
+            data.High.Add(122);
+            data.Low.Add(78);
+            data.Volume.Add(602);
+            Task<bool> exportTask = _exportService.ExportAsync(@".\onePageData.pdf", null, null);
+            exportTask.Wait();
+            bool result = exportTask.Result;
+            Assert.True(result);
         }
 
         private IMarketSelectionDataExportService _exportService = new PdfMarketSelectionExportService(new LoggerFactory());
