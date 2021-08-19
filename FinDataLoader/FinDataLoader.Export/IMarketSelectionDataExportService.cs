d05c556abaf8fcd9c6e39e9734023353d83d5a44
Ushakov Michael <um.nix.user@gmail.com>
2021-08-19 10:32:30 +0500

    export service

---
+++ FinDataLoader/FinDataLoader.Export/IMarketSelectionDataExportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FinDataLoader.Common.Data;
+
+namespace FinDataLoader.Export
+{
+    interface IMarketSelectionDataExportService
+    {
+        public Task<bool> ExportAsync(MarketSelection data, IDictionary<string, string> exportParams);
+    }
+}
---
+++ FinDataLoader/FinDataLoader.Export/Pdf/PdfMarketSelectionExportService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FinDataLoader.Common.Data;
+using FinDataLoader.Export;
+
+namespace FinDataLoader.Export.Pdf
+{
+    public class PdfMarketSelectionExportService : IMarketSelectionDataExportService
+    {
+        public Task<bool> ExportAsync(MarketSelection data, IDictionary<string, string> exportParams)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
