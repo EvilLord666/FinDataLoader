4465cd288ba018bb119b0b0c4a6fefd1d2748f91
Ushakov Michael <um.nix.user@gmail.com>
2021-08-20 10:45:42 +0500

    added data compressor

---
+++ FinDataLoader/FinDataLoader.Common/Services/IMarketSelectionDataCompressor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FinDataLoader.Common.Data;
+
+namespace FinDataLoader.Common.Services
+{
+    public enum CompessionOption
+    { 
+        Week = 1,
+        Month = 2,
+        Year = 3
+    }
+
+    public interface IMarketSelectionDataCompressor
+    {
+        public MarketSelection Compress(MarketSelection data, CompessionOption option);
+    }
+}
---
+++ FinDataLoader/FinDataLoader.Common/Services/SimpleMarketDataCompressor.cs
@@ -0,0 +1,15 @@
+﻿using FinDataLoader.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinDataLoader.Common.Services
+{
+    public class SimpleMarketDataCompressor : IMarketSelectionDataCompressor
+    {
+        public MarketSelection Compress(MarketSelection data, CompessionOption option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
