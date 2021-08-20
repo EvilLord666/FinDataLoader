af99134e2cef2f2cae050fb3e3b0b2e631984449
Ushakov Michael <um.nix.user@gmail.com>
2021-08-20 14:02:15 +0500

    created one more proj with tests 4 new service

---
+++ FinDataLoader/FinDataLoader.Common.Tests/Services/TestSimpleMarketDataCompressor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FinDataLoader.Common.Tests.Services
+{
+    public class TestSimpleMarketDataCompressor
+    {
+        [Fact]
+        public void TestCompressDataWeekly()
+        { 
+        
+        }
+
+        [Fact]
+        public void TestCompressDataMontly()
+        {
+
+        }
+    }
+}
