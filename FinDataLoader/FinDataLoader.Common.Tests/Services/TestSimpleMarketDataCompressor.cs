69fb15265fd5710236245be80d2e55e7e3a8da2d
Ushakov Michael <um.nix.user@gmail.com>
2021-08-20 16:16:26 +0500

    done

--- FinDataLoader/FinDataLoader.Common.Tests/Services/TestSimpleMarketDataCompressor.cs
+++ FinDataLoader/FinDataLoader.Common.Tests/Services/TestSimpleMarketDataCompressor.cs
@@ -58,7 +58,49 @@ namespace FinDataLoader.Common.Tests.Services
         [Fact]
         public void TestCompressDataMontly()
         {
+            MarketSelection data = new MarketSelection()
+            {
+                Timestamps = new List<DateTime>()
+                {
+                    new DateTime(2021, 6, 10),
+                    new DateTime(2021, 6, 13),
+                    new DateTime(2021, 7, 22),
+                    new DateTime(2021, 7, 29),
+                    new DateTime(2021, 8, 16) ,
+                    new DateTime(2021, 8, 18) ,
+                    new DateTime(2021, 8, 21)
+                },
 
+                Open = new List<decimal>()
+                {
+                    80, 84, 99, 102, 105, 100, 90
+                },
+
+                Close = new List<decimal>()
+                {
+                    85, 88, 100, 101, 107, 110, 112
+                },
+
+                High = new List<decimal>()
+                {
+                    104, 103, 120, 122, 125, 117, 129
+                },
+
+                Low = new List<decimal>()
+                {
+                    70, 72, 94, 91, 85, 88, 90
+                }
+            };
+
+            IList<CompressedMarketSelectionData> actualCompressedData = _compressor.Compress(data, CompessionOption.Month);
+            IList<CompressedMarketSelectionData> expectedCompressedData = new List<CompressedMarketSelectionData>()
+            {
+                new CompressedMarketSelectionData(new DateTime(2021, 6, 10), new DateTime(2021, 6, 13), 80, 88, 104, 70),
+                new CompressedMarketSelectionData(new DateTime(2021, 7, 22), new DateTime(2021, 7, 29), 99, 101, 122, 91),
+                new CompressedMarketSelectionData(new DateTime(2021, 8, 16), new DateTime(2021, 8, 21), 105, 112, 129, 85)
+            };
+
+            CompressedMarketSelectionDataChecker.Check(expectedCompressedData, actualCompressedData);
         }
 
         private IMarketSelectionDataCompressor _compressor = new SimpleMarketDataCompressor();
