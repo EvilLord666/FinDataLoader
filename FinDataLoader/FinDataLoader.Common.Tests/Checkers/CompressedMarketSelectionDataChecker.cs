37446ba52adf7dfe18a051f89b5724b4543defc3
Ushakov Michael <um.nix.user@gmail.com>
2021-08-20 16:05:01 +0500

    more complicated logic of a check

---
+++ FinDataLoader/FinDataLoader.Common.Tests/Checkers/CompressedMarketSelectionDataChecker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+using FinDataLoader.Common.Data;
+
+namespace FinDataLoader.Common.Tests.Checkers
+{
+    internal static class CompressedMarketSelectionDataChecker
+    {
+
+        public static void Check(IList<CompressedMarketSelectionData> expected, IList<CompressedMarketSelectionData> actual)
+        {
+            Assert.Equal(expected.Count, actual.Count);
+            foreach (CompressedMarketSelectionData e in expected)
+            {
+                CompressedMarketSelectionData a = actual.FirstOrDefault(item => item.Begin == e.Begin && item.End == e.End);
+                Assert.NotNull(a);
+                Check(e, a);
+            }
+        }
+
+        public static void Check(CompressedMarketSelectionData expected, CompressedMarketSelectionData actual)
+        {
+            Assert.Equal(expected.Begin, actual.Begin);
+            Assert.Equal(expected.End, actual.End);
+
+            Assert.Equal(expected.Open, actual.Open);
+            Assert.Equal(expected.Close, actual.Close);
+            Assert.Equal(expected.High, actual.High);
+            Assert.Equal(expected.Low, actual.Low);
+        }
+    }
+}
--- FinDataLoader/FinDataLoader.Common.Tests/Services/TestSimpleMarketDataCompressor.cs
+++ FinDataLoader/FinDataLoader.Common.Tests/Services/TestSimpleMarketDataCompressor.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FinDataLoader.Common.Data;
+using FinDataLoader.Common.Services;
+using FinDataLoader.Common.Tests.Checkers;
 using Xunit;
 
 namespace FinDataLoader.Common.Tests.Services
@@ -9,8 +12,47 @@ namespace FinDataLoader.Common.Tests.Services
     {
         [Fact]
         public void TestCompressDataWeekly()
-        { 
-        
+        {
+            MarketSelection data = new MarketSelection()
+            {
+                Timestamps = new List<DateTime>() 
+                { 
+                    new DateTime(2021, 8, 12), 
+                    new DateTime(2021, 8, 13), 
+                    new DateTime(2021, 8, 16) , 
+                    new DateTime(2021, 8, 18) , 
+                    new DateTime(2021, 8, 21) 
+                },
+
+                Open = new List<decimal>()
+                {
+                    99, 102, 105, 100, 90
+                },
+
+                Close = new List<decimal>()
+                {
+                    100, 101, 107, 110, 112
+                },
+
+                High = new List<decimal>()
+                { 
+                    120, 122, 125, 117, 129
+                },
+
+                Low = new List<decimal>()
+                { 
+                    94, 91, 85, 88, 90
+                }
+            };
+
+            IList<CompressedMarketSelectionData> actualCompressedData = _compressor.Compress(data, CompessionOption.Week);
+            IList<CompressedMarketSelectionData> expectedCompressedData = new List<CompressedMarketSelectionData>()
+            {
+                new CompressedMarketSelectionData(new DateTime(2021, 8, 12), new DateTime(2021, 8, 13), 99, 101, 122, 91),
+                new CompressedMarketSelectionData(new DateTime(2021, 8, 16), new DateTime(2021, 8, 21), 105, 112, 129, 85)
+            };
+
+            CompressedMarketSelectionDataChecker.Check(expectedCompressedData, actualCompressedData);
         }
 
         [Fact]
@@ -18,5 +60,7 @@ namespace FinDataLoader.Common.Tests.Services
         {
 
         }
+
+        private IMarketSelectionDataCompressor _compressor = new SimpleMarketDataCompressor();
     }
 }
