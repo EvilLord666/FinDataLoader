e00127ba483425b81a7d73dc8d1aef3a5f2563d7
Ushakov Michael <um.nix.user@gmail.com>
2021-08-20 12:55:36 +0500

    compressor done

--- FinDataLoader/FinDataLoader.Common/Services/SimpleMarketDataCompressor.cs
+++ FinDataLoader/FinDataLoader.Common/Services/SimpleMarketDataCompressor.cs
@@ -7,13 +7,26 @@ using System.Text;
 namespace FinDataLoader.Common.Services
 {
     internal class Range
-    { 
+    {
+        public Range(int begin, int end)
+        {
+            Begin = begin;
+            End = end;
+        }
+
         public int Begin { get; set; }
         public int End { get; set; }
     }
 
     public class SimpleMarketDataCompressor : IMarketSelectionDataCompressor
     {
+        public SimpleMarketDataCompressor()
+        {
+            _comparators[CompessionOption.Week] = CheckDatesAreFromSameWeek;
+            _comparators[CompessionOption.Month] = CheckDatesAreFromSameMonth;
+            _comparators[CompessionOption.Year] = CheckDatesAreFromSameYear;
+        }
+
         public MarketSelection Compress(MarketSelection data, CompessionOption option)
         {
             throw new NotImplementedException();
@@ -23,7 +36,36 @@ namespace FinDataLoader.Common.Services
         private IList<Range> GetIndexRanges(MarketSelection data, CompessionOption option)
         {
             IList<Range> ranges = new List<Range>();
+            int? beginIndex = null;
+            int? endIndex = null;
 
+            Func<DateTime, DateTime, bool> checker = _comparators[option];
+
+            for (int i = 0; i < data.Timestamps.Count; i++)
+            {
+                if (!beginIndex.HasValue)
+                {
+                    beginIndex = i;
+                }
+
+                if (i + 1 < data.Timestamps.Count)
+                {
+                    bool checkResult = checker(data.Timestamps[beginIndex.Value], data.Timestamps[i + 1]);
+                    if (checkResult)
+                    {
+                        endIndex = i + 1;
+                    }
+                    else
+                    {
+                        ranges.Add(new Range(beginIndex.Value, endIndex.Value));
+                        beginIndex = null;
+                    }
+                }
+                else
+                {
+                    ranges.Add(new Range(beginIndex.Value, i));
+                }
+            }
             return ranges;
         }
 
@@ -33,7 +75,7 @@ namespace FinDataLoader.Common.Services
             CalendarWeekRule weekRule = _cultureInfo.DateTimeFormat.CalendarWeekRule;
             DayOfWeek firstDayOfWeek = _cultureInfo.DateTimeFormat.FirstDayOfWeek;
             int initialDateWeekNumber = calendar.GetWeekOfYear(initial, weekRule, firstDayOfWeek);
-            int currentDateWeekNumber = calendar.GetWeekOfYear(initial, weekRule, firstDayOfWeek);
+            int currentDateWeekNumber = calendar.GetWeekOfYear(current, weekRule, firstDayOfWeek);
             return initialDateWeekNumber == currentDateWeekNumber;
         }
 
@@ -48,5 +90,7 @@ namespace FinDataLoader.Common.Services
         }
 
         private CultureInfo _cultureInfo = new CultureInfo("ru-RU");
+
+        private IDictionary<CompessionOption, Func<DateTime, DateTime, bool>> _comparators = new Dictionary<CompessionOption, Func<DateTime, DateTime, bool>>();
     }
 }
