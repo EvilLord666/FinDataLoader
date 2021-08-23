b8528deefcf13e92e4ca61754773fb68557b53e2
Ushakov Michael <um.nix.user@gmail.com>
2021-08-23 10:06:30 +0500

    Project replace + new project for dto's

---
+++ FinDataLoader/FinDataLoader.WebApi/Controllers/api/MarketSelectionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinDataLoader.Dto;
+
+namespace FinDataLoader.WebApi.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarketSelectionController : ControllerBase
+    {
+        [HttpGet("/api/market/yahoo/compressed")]
+        public async Task<IList<CompressedMarketSelectionDto>> getYahooCompressedMarketSelection()
+        {
+            return null;
+        }
+    }
+}
